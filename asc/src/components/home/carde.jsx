9862245d8cce1a46ec01e57512845a10f102dc9c
Evan junior <evsioljr@gmail.com>
2023-11-30 14:48:30 -0300

    estilo do grid

--- asc/src/components/home/carde.jsx
+++ asc/src/components/home/carde.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import "../../styles/card.css"
 
 function Carde(){
-    return(<>
+    return(
+    <>
           <a href="" className='link_card'><div className='card'>
             <h4>EDUCANDO</h4>
             <h4>NAS</h4>
