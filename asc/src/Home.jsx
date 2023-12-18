c173344bd1a38ce036885cbd567bcfbabc826d6d
Evan junior <evsioljr@gmail.com>
2023-12-18 08:30:00 -0300

    comit

--- asc/src/Home.jsx
+++ asc/src/Home.jsx
@@ -41,7 +41,7 @@ function Home() {
                 <div id='containertextimg'>
 
                     <img src={logo} id='logo' style={{ animation: animalogo }} />
-                    <details onClick={handledeta}>
+                    <details className='detail' onClick={handledeta}>
 
                         <ul className='logoText'>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui non amet
@@ -58,34 +58,35 @@ function Home() {
                 <a href="https://docs.google.com/document/d/1usIpFTbFV1Q3VZpbfoOorzn7Yz64MAUcv_8IAEVjzak/edit?usp=sharing" target="_blank"><button style={{}} className='butonsepara'>baixar ficha de filiação </button></a>
 
             </div>
-            <div style={{display:"flex",flexDirection:"column" ,alignItems:"center" , justifyContent:"center"}}>
-                <details onClick={handledeta} >
+            <div style={{display:"flex",flexDirection:"column" ,alignItems:"center" , justifyContent:"center",backgroundColor:"blue", width:"100vw" , paddingTop:"5vh"}}>
+                <details onClick={handledeta}  style={{color:"black", minHeight:"7vh"  }}>
 
-                    <ul className='logoText'>
-                        <p>
-                            Introdução
-                            A prática do surf é uma atividade física que tem crescido em
+                    <ul className='logoText' style={{fontSize:"13px", display:'flex', flexDirection:"column", alignItems:"center", justifyContent:"start", color:"black", padding:"4vw", backgroundColor:"white" , width:"90vw"}}>
+                        
+                            <h2>Introdução</h2>
+                            <p>A prática do surf é uma atividade física que tem crescido em
                             popularidade em todo o mundo, não só como esporte, mas também como
                             uma ferramenta de formação humana. No Brasil, a praia de Canavieiras,
                             com seus 13 km de costa, é ideal para a prática do surf, sendo escolhida como local de
                             competições de surf, picos de surf treino, e aulas da escolinha de surf Educando nas
-                            Ondas.
-                            Fundada no dia 25 de julho de 2023, a associação de surf Canavieirense nasce com o
+                            Ondas.</p><br />
+                            <p> Fundada no dia 25 de julho de 2023, a associação de surf Canavieirense nasce com o
                             objetivo de democratizar as oportunidades da prática do surf dentro e fora de Canavieiras,
                             promovendo ações que auxiliem os atletas dessa modalidade a se manterem ativos nesse
                             cenário esportivo, bem como, cumprir com às responsabilidades de preservação da
-                            natureza.
-                            Com o intuito de coordenar e organizar todos aspectos relacionados a prática de surf no
+                            natureza.</p> <br />
+                            <p>Com o intuito de coordenar e organizar todos aspectos relacionados a prática de surf no
                             município de Canavieiras, foram eleitos por votação o seguinte quadro de diretoria da
-                            instituição:
-                            Presidente: Juliano Fraga
-                            Vice presidente: Eduardo Messias
-                            Secretária: Eduarda Fernandes
-                            Tesoureiro: Daniel Pedreira
-                            Conselheiros Fiscais: Gustavo Souza e Daniel Amaral
-                            Suplentes: André Luiz Cassimiro e Tacyo Ribeiro
+                            instituição:</p><br />
+                           <p> Presidente: Juliano Fraga</p>
+                           <br />
+                            <p>Vice presidente: Eduardo Messias</p>
+                            <p>Secretária: Eduarda Fernandes</p>
+                            <p> Tesoureiro: Daniel Pedreira</p>
+                            <p> Conselheiros Fiscais: Gustavo Souza e Daniel Amaral</p>
+                            <p>Suplentes: André Luiz Cassimiro e Tacyo Ribeiro</p>
 
-                        </p>
+                        
                     </ul>
                 </details>
             </div>
